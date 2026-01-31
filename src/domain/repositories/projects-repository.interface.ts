850eaa1ace75cd9734747c8c13e044632dcf9b09
Danilo Silva <danilosilvap@outlook.com.br>
2026-01-31 17:32:54 -0300

    feat: add repositories implementations

---
+++ src/domain/repositories/projects-repository.interface.ts
@@ -0,0 +1,10 @@
+import { IProject } from '../interfaces/project.interface';
+import { DeepPartial } from 'typeorm';
+
+export interface IProjectsRepository {
+  findAll(userId: number): Promise<IProject[]>;
+
+  findById(id: number): Promise<IProject | null>;
+
+  add(payload: DeepPartial<IProject>): any;
+}
--- src/infrastructure/database/repositories/tasks.repository.service.ts
+++ src/infrastructure/database/repositories/tasks.repository.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource, DeepPartial, Repository } from 'typeorm';
+import { ProjectEntity } from '../entities/project.entity';
+import { TaskEntity } from '../entities/task.entity';
+import { ITasksRepository } from '../../../domain/repositories/tasks-repository.interface';
+import { ITask } from '../../../domain/interfaces/task.interface';
 
 @Injectable()
-export class TasksRepositoryService {}
+export class TasksRepositoryService
+  extends Repository<TaskEntity>
+  implements ITasksRepository
+{
+  constructor(dataSource: DataSource) {
+    super(ProjectEntity, dataSource.createEntityManager());
+  }
+
+  findAll(userId: number): Promise<ITask[]> {
+    return this.findBy({ user: { id: userId } });
+  }
+
+  findById(id: number): Promise<ITask | null> {
+    return this.findOneBy({ id });
+  }
+
+  add(payload: DeepPartial<ITask>) {
+    return this.save(payload);
+  }
+
+  updateById(id: number, payload: DeepPartial<ITask>) {
+    return this.update(id, payload);
+  }
+}
